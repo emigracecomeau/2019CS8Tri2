27cca0e61dfea998b039aa7dc30b3f3f48342487
emigracecomeau <ecomeau24@nscds.org>
2019-12-17 19:11:30 +0000

    Emoji Started

---
+++ EmojiColoring/coloring.js
@@ -0,0 +1,11 @@
+$(".pupilImage").click(
+function(){
+    alert("I wish it was Friday")
+}
+);
+
+$(".mouthImage").click(
+function(){
+    var clickedMouth = $(this).css("background-image");
+}
+);
